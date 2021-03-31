cbef846dfc7a2e9e2b5ad4f0bd963450814d6884
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-03-31 14:04:36 +0900

    hand in again

--- goya_false.rb
+++ goya_false.rb
@@ -1,7 +1,6 @@
 
 require 'cgi'
 cgi = CGI.new
-# データを受け取った後、HTMLの形式でレスポンスを返す
 cgi.out("type" => "text/html", "charset" => "UTF-8") {
   # 情報の目印とした"goya"のデータを cgi['goya']と言う記述で取り出し、ローカル変数に代入する
   get = cgi['goya_false']
